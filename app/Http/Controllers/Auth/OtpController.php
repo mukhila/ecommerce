8b537e6f0a7e5a28b7938e375f0edbb622d1a3ea
mukhila <mukhilaasokan@gmail.com>
2026-04-28 18:15:26 +0530

    latest update

--- app/Http/Controllers/Auth/OtpController.php
+++ app/Http/Controllers/Auth/OtpController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Services\SmsService;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
